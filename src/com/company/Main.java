53eb8e018bb9455eeef77e7529f4bd6c9507c89f
vex-xcc <vex.xcc@gmail.com>
2020-10-24 11:24:26 +0300

    add Comment to the main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    Customer ahmed = new Customer(10,"ahmed",20);
-	    Invoice ahmedInv =new Invoice(10,ahmed,100);
+
+		//Make an object of Customer class and gev it Id and name
+		//and discount
+		Customer ahmed = new Customer(10,"ahmed",20);
+		//Make an object of Invoice class and gev it Id and Customer name
+		//and total amount
+		Invoice ahmedInv = new Invoice(10,ahmed,100);
 		ahmedInv.printAll();
 	
 
